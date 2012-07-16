f2f4b626bdb1ada534e2ac70a88688ba58a93c5d
ralph <ralph@brooklynmarathon.com>
2012-07-16 10:27:10 -0400

    add login to home page

--- homepage.py
+++ homepage.py
@@ -4,6 +4,7 @@ import webapp2
 from datetime import datetime
 #from google.appengine.api import users
 from google.appengine.ext import db
+from google.appengine.api import users
 
 TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_environment = \
@@ -16,11 +17,19 @@ class ViewHomePage(webapp2.RequestHandler):
 #        user = users.get_current_user()
 
 #        if user:
-			template_values = {
-				'content1': html
-			}
-			template = jinja_environment.get_template('stdpage_block.html')
-			self.response.out.write(template.render(template_values))
+
+            logout = None
+            login = None
+            currentuser = users.get_current_user()
+            if currentuser:
+                  logout = users.create_logout_url('/notes/create' )
+            else:
+                  login = users.create_login_url('/notes/create')
+            template_values = {
+                'content1': html,'currentuser':currentuser, 'login':login, 'logout': logout
+            }
+            template = jinja_environment.get_template('stdpage_block.html')
+            self.response.out.write(template.render(template_values))
 
 #        else:
 #            self.redirect(users.create_login_url(self.request.uri))
